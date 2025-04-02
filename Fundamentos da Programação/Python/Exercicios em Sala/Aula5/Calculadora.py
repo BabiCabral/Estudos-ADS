54850dc1401f16abdbc3431308ae887e3d42c0de
Bárbara Cabral <99999337+BabiCabral@users.noreply.github.com>
2025-04-02 20:20:21 -0300

    Exercícios em Sala - Aula5 - Python

---
+++ Fundamentos da Programação/Python/Exercicios em Sala/Aula5/Calculadora.py
@@ -0,0 +1,5 @@
+A = int(input("Insira o primeiro valor: "))
+B = int(input("Insira o segundo valor: "))
+
+print("Soma: ", A+B, "\nSubtração: ", A-B, "\nMultiplicação: ", A*B, "\nDivisão: ", A/B)
+
---
+++ Fundamentos da Programação/Python/Exercicios em Sala/Aula5/CalculoDaMedia.py
@@ -0,0 +1,2 @@
+valor_dolar = float(input("Digite o valor em Dólar: "))
+print("Valor em real: R$ ", (valor_dolar/5.6))
