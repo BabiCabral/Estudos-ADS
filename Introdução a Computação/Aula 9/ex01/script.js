3cb13417d83bfc1b044d728ab3f3494efa632bc0
Bárbara Cabral <cabral.brm@gmail.com>
2025-05-16 21:17:57 -0300

    Aula 09

---
+++ Introdução a Computação/Aula 9/ex01/script.js
@@ -0,0 +1,15 @@
+let n1 = 3
+let n2 = 4
+let n3 =  5
+
+soma = Number(n1) + Number(n2)
+multi = Number(n1) + Number(n3)
+sub = Number(n3) - Number(n1)
+
+console.log(soma)
+console.log(multi/2)
+console.log(sub*-1)
+console.log(253%5)
+
+
+
---
+++ Introdução a Computação/Aula 9/script.js
@@ -0,0 +1,5 @@
+const idadeUsuario = prompt("Qual a sua idade?")
+const idadeMae = prompt("Qual idade da sua mãe?")
+
+// const somaIdades = idadeUsuario + idadeMae
+const somaIdades = Number(idadeUsuario) + Number(idadeMae)
