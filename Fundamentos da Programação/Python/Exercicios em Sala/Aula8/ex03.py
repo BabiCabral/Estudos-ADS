8b7521ab36224ee81e67dca012a52db0dd711d73
Bárbara Cabral <cabral.brm@gmail.com>
2025-05-17 17:18:31 -0300

    revisão exercicio matriz

--- Fundamentos da Programação/Python/Exercicios em Sala/Aula8/ex03.py
+++ Fundamentos da Programação/Python/Exercicios em Sala/Aula8/ex03.py
@@ -1,11 +1,22 @@
 # Adicionar valores em uma Matriz 2x4!
 # Exibir a matriz na tela.
 
-matriz = [[], []]
+matriz = [[],[]]
 
-for linha in range(2):
-    for coluna in range(4):
-        matriz[linha].append(int(input(f'Digite um valor para a posição [{linha}], [{coluna}]')))
+for i in range(2):
+    for j in range(4):
+        matriz[i].append(int(input(f"Insira os valores para a posição [[{i}],[{j}]]")))
 
-for linha in matriz:
-    print(linha)
+for i in matriz:
+    print(i)
+
+# ou
+
+# matriz = [[], []]
+
+# for linha in range(2):
+#     for coluna in range(4):
+#         matriz[linha].append(int(input(f'Digite um valor para a posição [{linha}], [{coluna}]')))
+
+# for linha in matriz:
+#     print(linha)
